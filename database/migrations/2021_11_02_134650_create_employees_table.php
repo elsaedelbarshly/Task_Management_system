9945755d52929d5b0a2312250ce80bfb7e416e92
elsaedelbarshly <eelbarshly@gmail.com>
2021-11-03 11:32:41 +0200

    migration

--- database/migrations/2021_11_02_134650_create_employees_table.php
+++ database/migrations/2021_11_02_134650_create_employees_table.php
@@ -18,7 +18,7 @@ class CreateEmployeesTable extends Migration
             $table->text('address');
             $table->string('education');
             $table->string('phone');
-            $table->timestamps('date_of_birth');
+            $table->date('date_of_birth');
             $table->unsignedBigInteger('user_id')->nullable();
             $table->unsignedBigInteger('organization_id')->nullable();
             $table->timestamps();
